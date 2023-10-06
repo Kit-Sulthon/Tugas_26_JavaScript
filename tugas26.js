d5b38a2edc729d1e9ccf9dbcef5f220840006fa0
T R A $ H <sulthons.a19@gmail.com>
2023-10-06 08:37:49 +0700

    Tugas 26 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas26.js
@@ -0,0 +1,27 @@
+function tugas26() {
+  var a = isFinite(2);
+  var b = isFinite(39);
+  var c = isFinite(76);
+  var d = isFinite(50);
+  var e = isFinite(9);
+  var f = isFinite(7);
+  var g = isFinite(41);
+  var h = isFinite(2);
+  var i = isFinite(24);
+  var j = isFinite(1);
+  var k = isFinite(16);
+
+  console.log("Angka 2", a, "Infinity.");
+  console.log("Angka 39", b, "Infinity.");
+  console.log("Angka 76", c, "Infinity.");
+  console.log("Angka 50", d, "Infinity.");
+  console.log("Angka 9", e, "Infinity.");
+  console.log("Angka 7", f, "Infinity.");
+  console.log("Angka 41", g, "Infinity.");
+  console.log("Angka 2", h, "Infinity.");
+  console.log("Angka 24", i, "Infinity.");
+  console.log("Angka 1", j, "Infinity.");
+  console.log("Angka 16", k, "Infinity.");
+}
+
+tugas26();
